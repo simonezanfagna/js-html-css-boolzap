c2dcaffe72f674ba17b7a9e391cee778c8c03330
simonezanfagna <zanfagnasimone@live.it>
2019-12-03 21:23:29 +0100

    inizio Milestone 2 ricerca utenti

--- main.js
+++ main.js
@@ -11,13 +11,15 @@ function invioMessage() {
 }
 
 function rispostaComputer() {
-  setTimeout(function () {
-    var risposta_computer = 'OK'
-    var template_testo = $('.template .message').clone();
-    template_testo.children('.message-text').text(risposta_computer);
-    template_testo.addClass('received');
-    $('.area-conversazione').append(template_testo);
-  },1000)
+  if (($('.area-conversazione').children().length) > 0 && !$('.area-conversazione').children().eq([-1]).hasClass('received')) {
+    setTimeout(function () {
+      var risposta_computer = 'OK';
+      var template_testo = $('.template .message').clone();
+      template_testo.children('.message-text').text(risposta_computer);
+      template_testo.addClass('received');
+      $('.area-conversazione').append(template_testo);
+    },1000)
+  }
 }
 
 $('.digita-testo input').keypress(function (event) {
@@ -30,18 +32,31 @@ $('.digita-testo input').keypress(function (event) {
 
 })
 
+$('.digita-testo input').keyup(function (event) {
+  var nuovo_testo = $('.digita-testo input').val();
+  if (nuovo_testo.length != 0) {
+    $('#microphone').removeClass('active');
+    $('#paper-plane').addClass('active');
+  }
+  else {
+    $('#paper-plane').removeClass('active');
+    $('#microphone').addClass('active');
+  }
+})
+
 $('.digita-testo .fas').click(function () {
   invioMessage();
+  rispostaComputer();
   $('#paper-plane').removeClass('active');
   $('#microphone').addClass('active');
-  rispostaComputer();
 })
 
 $('.digita-testo input').keypress(function (event) {
   if (event.which == 13) {
     invioMessage();
+    rispostaComputer();
     $('#paper-plane').removeClass('active');
     $('#microphone').addClass('active');
-    rispostaComputer();
   }
 })
+console.log($('.area-conversazione').children().length);
